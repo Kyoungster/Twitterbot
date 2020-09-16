95621106152f7c569b3ce46e3d3d0de48783dae8
Kyoungster <20803645+Kyoungster@users.noreply.github.com>
2020-09-16 09:50:38 -0400

    Update Auto-Like.py

--- Auto-Like.py
+++ Auto-Like.py
@@ -8,7 +8,7 @@ CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
-USER_LIST = environ['USER_LIST']
+USER_LIST = os.environ.get('USER_LIST').split(",")
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 
